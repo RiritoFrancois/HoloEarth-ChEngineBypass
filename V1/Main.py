841c90e229b9646324e63df6a436079727ebfd6d
Ririto Ninigaya <154670403+RiritoNinigaya@users.noreply.github.com>
2023-12-29 01:14:17 +0200

    Fixed Rename DLL Lib

--- V1/Main.py
+++ V1/Main.py
@@ -3,7 +3,7 @@ import os
 fullway_holoearth = "E:\\COVER corp\\HoloearthApps\\Holoearth\\Holoearth_Data\\Plugins\\x86_64\\KS_Diagnostics_Process.dll"
 renamed = "E:\\COVER corp\\HoloearthApps\\Holoearth\\Holoearth_Data\\Plugins\\x86_64\\Renamed_KS_Diagnostics_Process.dll"
 def Main():
-    os.rename(fullway_holoearth, os.environ['COVERCORPHOLOEARTH'] + r"\\KSVbV.dll")
+    os.rename(fullway_holoearth, renamed)
     os.remove(fullway_holoearth)
 if __name__ == "__main__":
     Main()
